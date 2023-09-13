a7a9cf9c25f654c36c7c943304b7a8fd834635b9
vendannotes <111574533+i-am-millionaire@users.noreply.github.com>
2023-09-13 09:47:42 +0530

    Update and rename Machine.sh to Machine.txt

--- Machine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user XMR:4796GCFi2PbTQSv1xHHgetSkHdzLQ3fcT6BkFP5uQFM4bbfvJdAiC41eLakB5J7NKaKZogLfBzVeR64wvHREm8pUS3i4ohk.Time1 --ethstratum ETHPROXY
